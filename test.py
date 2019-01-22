2081d6282e3d17109b57425c463bbcb387765d42
ly1996 <2958058357@qq.com>
2019-01-23 00:35:27 +0800

    rm mistakes

--- test.py
+++ test.py
@@ -41,7 +41,7 @@ max_change_below = original_image - 0.01
 hacked_image = np.copy(original_image)
 
 batch_shape = [1, 299 , 299, 3]
-grad = tf.zeros(shape=batch_shape)
+grad = np.zeros(shape=batch_shape)
 momentum = 1.0
 alpha = (2.0 * 16.0 / 255.0) / 10
 
@@ -79,6 +79,7 @@ for i in range(10):
     noise = gradients
     # noise = gradients / tf.reduce_mean(tf.abs(gradients), [1, 2, 3], keep_dims=True)
     noise = momentum * grad + noise
+    print (type(noise))
     hacked_image = hacked_image + alpha * np.sign(noise)
 
     grad = noise
