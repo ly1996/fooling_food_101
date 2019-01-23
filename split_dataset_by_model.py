5a724c1db0032d56e50deb41570b71ec7826f944
ly1996 <2958058357@qq.com>
2019-01-23 10:52:53 +0800

    rm mistakes

--- split_dataset_by_model.py
+++ split_dataset_by_model.py
@@ -1,9 +1,22 @@
 import os
+from tensorflow.keras.models import load_model
+from PIL import Image
 
 input_dir = os.path.expanduser("~/winter-camp-pek/food-101/food-101/images")
 model_path = os.path.expanduser("~/winter-camp-pek/tmp/fooling_food_101/checkpoint-12-1.7504.hdf5")
+model = load_model(model_path)
+
+def is_correct(model,img,i):
+
 
 for root,dirs,files in os.walk(input_dir):
     dirs.sort()
     print(dirs)
+    for i in range(len(dirs)):
+        dir = dirs[i]
+        sub_dir = os.path.join(input_dir,dir)
+        print (sub_dir)
+        for file in os.listdir(sub_dir):
+            # print(file)
+            img = Image.open(os.path.join(sub_dir, file))
     break
