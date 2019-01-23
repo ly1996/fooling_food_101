b80201c80c36441cbf6575be8b4b8d03ccab5e2d
ly1996 <2958058357@qq.com>
2019-01-23 10:43:56 +0800

    rm mistakes

--- split_dataset_by_model.py
+++ split_dataset_by_model.py
@@ -4,4 +4,6 @@ input_dir = os.path.expanduser("~/winter-camp-pek/food-101/food-101/images")
 model_path = os.path.expanduser("~/winter-camp-pek/tmp/fooling_food_101/checkpoint-12-1.7504.hdf5")
 
 for root,dirs,files in os.walk(input_dir):
+    dirs.sort()
     print(dirs)
+    break
