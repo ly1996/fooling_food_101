1d79c88ebf709dee9d2aedc565ed73a5445087f2
ly1996 <2958058357@qq.com>
2019-01-23 20:32:39 +0800

    rm 15

--- generate_noise_image.py
+++ generate_noise_image.py
@@ -186,7 +186,8 @@ for root,dirs,files in os.walk(input_dir):
         #cannoli:12
         #caprese_salad:13
         #carrot_cake:14
-        if i in [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14]:
+        #ceviche:15
+        if i in [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]:
             continue
         dir = dirs[i]
 
