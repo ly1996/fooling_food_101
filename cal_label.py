b12aa5099cd0668289e7c67d3e4ce5c2d97796b9
ly1996 <2958058357@qq.com>
2019-01-24 12:19:53 +0800

    test inception

--- cal_label.py
+++ cal_label.py
@@ -2,11 +2,26 @@ import os
 from random import randint
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
-from tensorflow.keras.applications.resnet50 import ResNet50, preprocess_input
+# from tensorflow.keras.applications.resnet50 import ResNet50, preprocess_input
+from tensorflow.keras.applications.inception_v3 import InceptionV3, preprocess_input
 import numpy as np
 
 model_path = os.path.expanduser("~/winter-camp-pek/tmp/fooling_food_101/checkpoint-54-1.1064.hdf5")
+inception_model_path = os.path.expanduser("~/winter-camp-pek/tmp/fooling_food_101/checkpoint-inception-64-1.0762.hdf5")
 model = load_model(model_path)
+
+def get_label_inception(img_path):
+    img = image.load_img(img_path,
+                         target_size=(299, 299))
+    original_image = image.img_to_array(img)
+    original_image = preprocess_input(original_image)
+    original_image = np.expand_dims(original_image, axis=0)
+
+    preds = model.predict(original_image)
+    pred = preds[0]
+    label = np.argmax(pred)
+
+    return label
 
 def get_label(img_path):
     img = image.load_img(img_path,
@@ -49,7 +64,7 @@ for root,dirs,files in os.walk(final_dir):
 
         for file in os.listdir(sub_dir):
             count += 1
-            predict_class = get_label(os.path.join(normal_sub_dir, file))
+            predict_class = get_label_inception(os.path.join(normal_sub_dir, file))
             if predict_class == i:
                 count_correct += 1
 
